8065965fca3ad4266d3c0ed174cec2b370652fa9
MehakK1311 <mehakhanna1311@gmail.com>
2023-11-29 02:41:27 +0530

    feat: resend otp

--- api/User.js
+++ api/User.js
@@ -218,216 +218,24 @@ router.post("/verify-otp", async (req, res)=>{
   }
 })
 
-//send verification email function
-// const sendVerificationEmail = ({ _id, email, name }, res) => {
-//   const dev = "http://localhost:3000/";
-//   const prod = "https://email-otp-verification-backend-me92.onrender.com/";
-//   const currentUrl = process.env.NODE_ENV ? prod : dev;
+//resend
+router.post('/resend-otp', async(req, res) => {
+  try{
+    let{userId, email} = req.body
 
-//   const uniqueString = uuidv4() + _id;
-
-//   const mailOption = {
-//     from: process.env.AUTH_EMAIL,
-//     to: email,
-//     subject: "Verify Your Email",
-//     html: `<h2>Email Verification</h2>
-
-//     <p>Hello ${name},</p>
-
-//     <p>Thank you for signing up! To complete your registration, please click the link below to verify your email:</p>
-
-//     <p><a href=${currentUrl + "user/verify/" + _id + "/" + uniqueString
-//       } style="text-decoration: none; background-color: #4CAF50; color: white; padding: 10px 15px; border-radius: 5px; display: inline-block;">Verify Email</a></p>
-
-//     <p>If you did not sign up for this account, you can ignore this email.</p>
-//     <p><b>Link expires in 6 hours</b></p>
-
-//     <p>Thank you</p>`,
-//   };
-
-//   // hash unique string
-//   const saltRounds = 10;
-//   bcrypt
-//     .hash(uniqueString, saltRounds)
-//     .then((hashedUniqueString) => {
-//       // set values in userVerification collection
-//       const newVerification = new UserVerification({
-//         userId: _id,
-//         uniqueString: hashedUniqueString,
-//         createdAt: Date.now(),
-//         expiresAt: Date.now() + 21600000,
-//       });
-
-//       newVerification
-//         .save()
-//         .then(() => {
-//           transporter
-//             .sendMail(mailOption)
-//             .then(() => {
-//               // Email sent successfully
-//               console.log("Verification email sent!");
-//               // Do not send a response here, as it's already being handled by the calling function
-//             })
-//             .catch((err) => {
-//               // Handle email sending failure
-//               console.error(err);
-//               // Send a response indicating email failure
-//               res.json({
-//                 status: "FAILED",
-//                 message: "Verification email failed!",
-//               });
-//             });
-//         })
-//         .catch((err) => {
-//           // Handle database saving failure
-//           console.error(err);
-//           // Send a response indicating database failure
-//           res.json({
-//             status: "FAILED",
-//             message: "Couldn't save verification email data!",
-//           });
-//         });
-//     })
-//     .catch(() => {
-//       // Handle hashing failure
-//       console.error("An error occurred while hashing email data!");
-//       // Send a response indicating hashing failure
-//       res.json({
-//         status: "FAILED",
-//         message: "An error occurred while hashing email data!",
-//       });
-//     });
-// };
-
-//resend verification email
-// router.post('/resend-verification-link', async(req, res)=>{
-//   try{
-//     let {userId, email}=req.body;
-
-//     if(!userId||!email){
-//       throw Error("Empty user details not allowed")
-//     }else{
-//       await UserVerification.deleteMany({userId:userId, email:email});sendVerificationEmail({_id:userId, email}, res);
-//     }
-//   }catch(err){
-//     res.json({
-//       status:'FAILED',
-//       message:`Verification Link resend Failed. ${err.message}`
-//     })
-//   }
-// })
-
-//verify email
-// router.get("/verify/:userId/:uniqueString", (req, res) => {
-//   let { userId, uniqueString } = req.params;
-
-//   UserVerification.find({ userId })
-//     .then((result) => {
-//       if (result.length > 0) {
-//         //if verification data exists
-
-//         const { expiresAt } = result[0];
-//         const hashedUniqueString = result[0].uniqueString;
-
-//         if (expiresAt < Date.now()) {
-//           //record expired to we delete it
-//           UserVerification.deleteOne({ userId })
-//             .then((result) => {
-//               User.deleteOne({ _id: userId })
-//                 .then(() => {
-//                   let message = "Link has expires. Please sign up again";
-//                   return res.redirect(
-//                     `/user/verified/error=true&message=${message}`
-//                   );
-//                 })
-//                 .catch((error) => {
-//                   console.log(error);
-//                   let message = "Cleaning user with expired string failed";
-//                   return res.redirect(
-//                     `/user/verified/error=true&message=${message}`
-//                   );
-//                 });
-//             })
-//             .catch((err) => {
-//               console.error(err);
-//               let message =
-//                 "An error occurred while clearing expired user verification email";
-//               return res.redirect(
-//                 `/user/verified/error=true&message=${message}`
-//               );
-//             });
-//         } else {
-//           //valid record exists so we validate the user string
-
-//           //compare hashed uniqus string
-
-//           bcrypt
-//             .compare(uniqueString, hashedUniqueString)
-//             .then((result) => {
-//               if (result) {
-//                 //string match
-
-//                 User.updateOne({ _id: userId }, { verified: true })
-//                   .then(() => {
-//                     UserVerification.deleteOne({ userId })
-//                       .then(() => {
-//                         res.sendFile(
-//                           path.join(__dirname, "../views/verified.html")
-//                         );
-//                       })
-//                       .catch((err) => {
-//                         console.error(err);
-//                         let message =
-//                           "An error occurred while finalizing successful validation";
-//                         return res.redirect(
-//                           `/user/verified/error=true&message=${message}`
-//                         );
-//                       });
-//                   })
-//                   .catch((error) => {
-//                     let message =
-//                       "An error occurred while updating user verification";
-//                     return res.redirect(
-//                       `/user/verified/error=true&message=${message}`
-//                     );
-//                   });
-//               } else {
-//                 //record is incorrect
-//                 let message = "Invalid details passed. Check your inbox";
-//                 return res.redirect(
-//                   `/user/verified/error=true&message=${message}`
-//                 );
-//               }
-//             })
-//             .catch((err) => {
-//               let message = "An error occurred while comparing unique string";
-//               return res.redirect(
-//                 `/user/verified/error=true&message=${message}`
-//               );
-//             });
-//         }
-//       } else {
-//         let message =
-//           "Account record doesnt exist or has been verified already. Please sign up or login";
-//         return res.redirect(`/user/verified/error=true&message=${message}`);
-//       }
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       let message =
-//         "An error occurred while checking existing user verification email";
-//       return res.redirect(`/user/verified/error=true&message=${message}`);
-//     });
-// });
-
-//verified page route
-// router.get("/verified", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../views/verified.html"));
-// });
-
-
-
-
+    if(!userId || !email){
+      throw new Error('Empty user details not allowed')
+    }else{
+      await UserOtpVerification.deleteMany({userId})
+      sendOtpVerificationEmail({_id: userId, email}, res)
+    }
+  }catch(error){
+    res.json({
+      status:'FAILED',
+      message:error.message
+    })
+  }
+})
 
 //signin
 router.post("/signin", (req, res) => {
